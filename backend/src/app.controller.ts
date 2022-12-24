bdb4cab26e8bc240fdd583e0290d2f14b138ac1b
Oscar-FV <a280210@alumnos.uaslp.mx>
2022-12-23 18:04:02 -0600

    call to the weather api

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/:q/:days/:aqi')
+  getForecast(@Param('q') q:string, @Param('days') days:string, @Param('aqui') aqui:string){
+    return this.appService.getForecast(q, days, aqui);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios/dist';
+import { map } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  constructor(private httpService: HttpService){}
+
+  getForecast(q: string, days:string, aqui:string) {
+    return this.httpService
+      .get(`http://api.weatherapi.com/v1/current.json?key=2a582530dab2430bab4183006221612&q=Mexico, San Luis Potosi&aqi=yes`)
+      .pipe(
+        map((response)=> response.data)
+        // map((data)=> ({
+          
+        // })
+      )
   }
 }
